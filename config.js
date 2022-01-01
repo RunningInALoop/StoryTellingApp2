02b050b0a8d6e7f6399acde3936a0c5d616bedcd
RinseRepeat <73837851+RunningInALoop@users.noreply.github.com>
2022-01-01 15:14:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBihdqSN5nPqlkHKfCre-hlmKfxq3RZmiM",
+  authDomain: "story-telling-app2.firebaseapp.com",
+  databaseURL: "https://story-telling-app2-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app2",
+  storageBucket: "story-telling-app2.appspot.com",
+  messagingSenderId: "930854893126",
+  appId: "1:930854893126:web:a47c34a754f86ffc617c47"
+};
